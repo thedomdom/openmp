a85e320d8ecf05ebe377b0e42ed69de1b0d3c02c
Domi W <uiuqb@student.kit.edu>
2019-10-01 17:06:10 +0200

    Hello World OpenMP

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include <printf.h>
+#include <iostream>
+#include <omp.h>
 
 int main() {
-#pragma omp parallel
+#pragma omp parallel num_threads(4) default(none)
     {
-        int ID = 0;
+        int ID = omp_get_thread_num();
         printf("hello(%d) ", ID);
         printf("world(%d) \n", ID);
     }
 }
+#pragma clang diagnostic pop
